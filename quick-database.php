6e322a6196cb13c15772b68ed68e0183cdf24e93
Andros Fenollosa <andros@fenollosa.email>
2019-01-22 01:05:20 +0100

    Add animate background

--- quick-database.php
+++ quick-database.php
@@ -35,9 +35,70 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <title>Quick-database | Fast database creator</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="author" content="Andros Fenollosa">			
+        <script>
+            document.addEventListener('DOMContentLoaded', () => {
+                let colors = [
+                    [9,35,39],
+                    [11,83,81],
+                    [0,169,165],
+                    [78,128,152],
+                    [144,194,231],
+                    [255,128,0]
+                ];
+
+                let step = 0;
+                // current color left
+                // next color left
+                // current color right
+                // next color right
+                let colorIndices = [0,1,2,3];
+
+                //transition speed
+                let gradientSpeed = 0.002;
+
+                function updateGradient() {
+                let c0_0 = colors[colorIndices[0]];
+                let c0_1 = colors[colorIndices[1]];
+                let c1_0 = colors[colorIndices[2]];
+                let c1_1 = colors[colorIndices[3]];
+
+                let istep = 1 - step;
+                let r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+                let g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+                let b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+                let color1 = "rgb("+r1+","+g1+","+b1+")";
+
+                let r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+                let g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+                let b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+                let color2 = "rgb("+r2+","+g2+","+b2+")";
+
+                document.documentElement.style.setProperty('--color-gradient-1', color1);
+                document.documentElement.style.setProperty('--color-gradient-2', color2);
+                  
+                  step += gradientSpeed;
+                  if ( step >= 1 )
+                  {
+                    step %= 1;
+                    colorIndices[0] = colorIndices[1];
+                    colorIndices[2] = colorIndices[3];
+                    
+                    //pick two new target color indices
+                    //do not pick the same as the current one
+                    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+                    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+                    
+                  }
+                }
+
+                setInterval(updateGradient, 10);
+            });
+        </script>
         <style type="text/css">
 
             :root {
+                --color-gradient-1: #fff;
+                --color-gradient-2: #fff;
                 --color-black: #000000;
                 --color-blue: #14213d;
                 --color-platinum: #e5e5e5;
@@ -48,7 +109,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             body {
                 margin: 0;
-                background-image: linear-gradient(to bottom right, var(--color-platinum), var(--color-blue));
+                background-image: linear-gradient(to bottom right, var(--color-gradient-1), var(--color-gradient-2));
                 font-family: Arial;
             } 
 
